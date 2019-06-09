29fcb7515b7650c9d00281ded064ecad3697d173
N1Boldyrev <fraidzilla@gmail.com>
2019-06-09 15:03:49 +0300

    pdo_deploy

--- pdo.php
+++ pdo.php
@@ -1,10 +1,10 @@
 <?php
-$host='localhost';
-$dbname='aquarium';
+$host='172.23.64.64';
+$dbname='db_2222756';
 $charset='utf8';
 $dsn="mysql:host=$host;dbname=$dbname;charset=$charset";
 $options=[
    PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION,
    PDO::ATTR_DEFAULT_FETCH_MODE=>PDO::FETCH_ASSOC
          ];
-$pdo=new PDO($dsn,'root','',$options);
+$pdo=new PDO($dsn,'2222756','1597534568',$options);
