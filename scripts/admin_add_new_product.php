7ec6715facf9160962e41d9a6824dcdd7f7e5d68
N1Boldyrev <fraidzilla@gmail.com>
2019-06-20 23:36:30 +0300

    change product segment

--- scripts/admin_add_new_product.php
+++ scripts/admin_add_new_product.php
@@ -7,18 +7,18 @@ $description=$_GET["description"];
 $image_dir="../imgs/description_imgs";
 
 if(!$type||!$name||!$price||!$description){
-    echo "<h1>Вы не заполнили все данные формы</h1>"
+    echo "<h1>Вы не заполнили все данные формы</h1>";
    return;
 }
 else{
     include "../pdo.php";
-$stmt=$pdo->query("INSERT INTO product VALUES(NULL,".$current_waybill_id.",'".$type."','".$name."','".$price."','".$description."')");
+$stmt=$pdo->query("INSERT INTO product VALUES(NULL,'".$type."','".$name."','".$price."','".$description."')");
 $stmt=$pdo->query("SELECT LAST_INSERT_ID();");
 foreach($stmt as $q){
     $current_product_id=$q["LAST_INSERT_ID()"];
 }
 
-echo "<h3>Товар добавлен. Для добавления изображения, добавте файл с изображением ".$current_product_id.".jpg</h3>";
+echo "<h3>Товар добавлен. Имя для изображения товара ".$current_product_id.".jpg</h3>";
 
 }
 ?>
